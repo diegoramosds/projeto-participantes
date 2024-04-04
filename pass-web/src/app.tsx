0a0e24958e2698d41c9968fc75784f9d2d5373d9
Diego <diegoramos3709@gmail.com>
2024-04-04 16:23:42 -0300

    Adicionando api e aplicação

---
+++ pass-web/src/app.tsx
@@ -0,0 +1,13 @@
+import { AteendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
+
+export function App() {
+return (
+  <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+    <Header />
+    <AteendeeList />
+  </div>
+)
+}
+
